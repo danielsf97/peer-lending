fca7ec527b908c6908705f9013c15750d6944e4b
Helena Poleri <a78633@alunos.uminho.pt>
2019-01-06 22:31:52 +0000

    :books: Mais comentários para o cliente wow

--- Client/src/main/java/ClientType.java
+++ Client/src/main/java/ClientType.java
@@ -1,5 +1,10 @@
 import java.util.ArrayDeque;
 
+
+/**
+ * Representa um cliente.
+ *
+ */
 public class ClientType {
     private String name;
     private boolean loggedIn;
@@ -7,6 +12,12 @@ public class ClientType {
     private int numAsyncMessages;
     private Protos.MessageWrapper syncMessage;
 
+
+    /**
+     * Representa um cliente.
+     *
+     * @param name
+     */
     public ClientType(String name) {
         this.name = name;
         this.loggedIn = true;
@@ -15,32 +26,73 @@ public class ClientType {
         this.syncMessage = null;
     }
 
+
+    /**
+     * Devolve o nome do cliente.
+     *
+     * @return  Nome do cliente.
+     */
     public String getName() {
         return name;
     }
 
-    public boolean isLoggedIn(){
+
+    /**
+     * Devolve se o cliente está autenticado ou não.
+     *
+     * @return true caso o cliente esteja autenticado
+     *         falso caso contrário.
+     */
+    public boolean isLoggedIn() {
         return this.loggedIn;
     }
 
-    public void logout(){
+
+    /**
+     * Faz logout a um cliente.
+     *
+     */
+    public void logout() {
         this.loggedIn = false;
     }
 
+
+    /**
+     * Retorna o número de mensagens assíncronas.
+     *
+     * @return o número de mensagens assíncronas.
+     */
     public synchronized int getNumAsyncMessages() {
         return numAsyncMessages;
     }
 
+    /**
+     * Adiciona uma mensagem assíncrona (notificação).
+     *
+     * @param asyncMsg mensagem assíncrona a adicionar.
+     */
     public synchronized void addAsyncMessage(String asyncMsg) {
         this.asyncMessages.addLast(asyncMsg);
         this.numAsyncMessages++;
     }
 
+
+    /**
+     * Estabelece uma mensagem síncrona recebida.
+     *
+     * @param syncMessage Mensagem recebida.
+     */
     public synchronized void setSyncMessage(Protos.MessageWrapper syncMessage) {
         this.syncMessage = syncMessage;
         this.notify();
     }
 
+
+    /**
+     * Espera por uma mensagem síncrona, retornando-a.
+     *
+     * @return   mensagem síncrona recebida.
+     */
     public synchronized Protos.MessageWrapper getSyncMessage() {
         Protos.MessageWrapper msg;
         try {
@@ -59,6 +111,12 @@ public class ClientType {
         return msg;
     }
 
+
+    /**
+     * Lista de mensagens assíncronas.
+     *
+     * @return  lista de mensagens assíncronas.
+     */
     public synchronized String getAsyncMessages() {
 
         String msg;
--- Client/src/main/java/Investor.java
+++ Client/src/main/java/Investor.java
@@ -1,25 +1,52 @@
 import java.util.ArrayDeque;
 
+/**
+ * Representa um investidor.
+ *
+ */
 public class Investor extends ClientType {
 
     private ArrayDeque<String> notifications;
     private int numNotifications;
 
+    /**
+     * Construtor parametrizado.
+     *
+     * @param name  Nome do investidor.
+     */
     public Investor(String name) {
         super(name);
         this.notifications = new ArrayDeque<>();
         this.numNotifications = 0;
     }
 
+
+    /**
+     * Adiciona uma notificação para o investidor.
+     *
+     * @param notification  Notificação a adicionar.
+     */
     public synchronized void addNotification(String notification) {
         this.notifications.addLast(notification);
         this.numNotifications++;
     }
 
+
+    /**
+     * Retorna número de notificações para o investidor.
+     *
+     * @return número de notificações para o investidor.
+     */
     public synchronized int getNumNotifications() {
         return numNotifications;
     }
 
+
+    /**
+     * Retorna as notificações para um investidor.
+     *
+     * @return as notificações para um investidor.
+     */
     public synchronized String getNotifications() {
         String msg;
         StringBuilder sb = new StringBuilder();
