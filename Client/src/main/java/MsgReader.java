7c629fd822b83f9dc35a4b05a7a8577b033a9c27
Helena Poleri <a78633@alunos.uminho.pt>
2019-01-06 22:54:11 +0000

    :books: Só mais este

--- Client/src/main/java/MsgReader.java
+++ Client/src/main/java/MsgReader.java
@@ -1,21 +1,39 @@
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
 
+
+/**
+ * Responsável por ler mensagens recebidas por um cliente.
+ *
+ */
 public class MsgReader extends Thread {
     private SocketChannel sc;
     private ClientType client;
 
+
+    /**
+     * Construtor parametrizado.
+     *
+     * @param socket    Socket.
+     * @param client    Cliente a quem são destinadas as mensagens.
+     */
     public MsgReader(SocketChannel socket, ClientType client) {
         this.sc = socket;
         this.client = client;
     }
 
+
+    /**
+     * Lê mensagens recebidas, síncronas e assíncronas. Adiciona as mensagens
+     * ao respetivo cliente.
+     *
+     */
     public void run() {
         byte[] msgB;
         Protos.MessageWrapper msg;
         try {
 
-            while ((msgB = Utils.recvMsg(sc)) != null) {
+            while((msgB = Utils.recvMsg(sc)) != null) {
 
                 msg = Protos.MessageWrapper.parseFrom(msgB);
                 switch (msg.getMsgType()){
