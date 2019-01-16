aca54020009834206c240d1b8a72a13eafe3cbbc
Daniel Fernandes <danielsf97@gmail.com>
2019-01-16 15:20:13 +0000

    comentários Frontend

--- Frontend/frontend/src/receiver.erl
+++ Frontend/frontend/src/receiver.erl
@@ -1,22 +1,48 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2018-2019, Paradigmas de Sistemas Distribuídos
+%%% @doc Modulo que cria um ator Receiver associado a uma exchange. 
+%%%
+%%% Responsável por inicializar um ator cuja função é a de receber mensagens
+%%% das exchanges e retorna-las ao ator responsável pela sessão do cliente
+%%% respetivo.
+%%% @author Grupo 3
+%%% @end
+%%%-----------------------------------------------------------------------------
 -module(receiver).
 -export([start/2]).
 
+%%------------------------------------------------------------------------------
+%% @doc Função que inicializa um ator Receiver.
+%%
+%% Faz bind de um socket pull no qual a exchange respetiva deverá se conectar e
+%% enviar todas as mensagens destinadas aos clientes.
+%% @end
+%%------------------------------------------------------------------------------
 start(Host, Port) ->
 	{ok,Context} = erlzmq:context(),
 	{ok, Socket} = erlzmq:socket(Context, [pull, {active, false}]),
 	ok = erlzmq:bind(Socket, "tcp://" ++ Host ++ ":" ++ Port),
 	spawn(fun() -> msg_forwarding_loop(Socket) end).
 
+%%------------------------------------------------------------------------------
+%% @doc Função que contitui o ciclo de vida de um receiver.
+%%
+%% Processa cada uma das mensagens que lhe chegam da exchange associada,
+%% fazendo-as chegar ao ator responsável pela sessão do cliente.
+%% @end
+%%------------------------------------------------------------------------------
 msg_forwarding_loop(Socket) ->
 	case erlzmq:recv(Socket) of
 		{ok, Bin} ->
 			case protos_pb:decode_msg(Bin, 'MessageWrapper') of
 				{'MessageWrapper', _, undefined, Msg} ->
+					io:format("Recebeu Mensagem sem Token!"),
 					{auctionemissionresult, Body} = Msg,
 					{'AuctionEmissionResult', Client_B, _} = Body,
 					Client = binary_to_list(Client_B),
 					send_to_client_session(Client, Bin);
 				{'MessageWrapper', _, BinToken, _} ->
+					io:format("Recebeu Mensagem com Token!"),
 					Token = binary_to_list(BinToken),
 					ClientSessionPid = list_to_pid(Token),
 					ClientSessionPid ! {receiver, Bin}
@@ -25,6 +51,14 @@ msg_forwarding_loop(Socket) ->
 			msg_forwarding_loop(Socket)
 	end.
 
+%%------------------------------------------------------------------------------
+%% @doc Função responsável por enviar uma mensagem assíncrona a uma cliente.
+%%
+%% Mensagens assíncronas não possuem o PID do ator responsável pela sessão do
+%% cliente, pelo que antes de enviar a mensagem tem de contacta o login_manager
+%% de forma a conhecer o PID correto.
+%% @end
+%%------------------------------------------------------------------------------
 send_to_client_session(Client, Msg) ->
 
 	case login_manager:get_pid(Client) of
--- Frontend/frontend/src/sender.erl
+++ Frontend/frontend/src/sender.erl
@@ -1,12 +1,36 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2018-2019, Paradigmas de Sistemas Distribuídos
+%%% @doc Modulo que cria um ator Sender associado a uma exchange. 
+%%%
+%%% Responsável por inicializar um ator cuja função é a de perante cada mensagem
+%%% que chegue vinda do ator responsável pela sessão do cliente, a encaminhar
+%%% para a exchange a que se encontra associado.
+%%% @author Grupo 3
+%%% @end
+%%%-----------------------------------------------------------------------------
 -module(sender).
 -export([start/2]).
 
+%%------------------------------------------------------------------------------
+%% @doc Função que inicializa um ator Sender.
+%%
+%% Faz connect ao socket pull da exchange respetiva e para o qual deverá enviar
+%% todas as mensagens. Inicializa o ciclo de vida do ator.
+%% @end
+%%------------------------------------------------------------------------------
 start(Host, Port) ->
 	{ok,Context} = erlzmq:context(),
 	{ok, Socket} = erlzmq:socket(Context, push),
 	ok = erlzmq:connect(Socket, "tcp://" ++ Host ++ ":" ++ Port),
 	spawn(fun() -> msg_forwarding_loop(Socket) end).
 
+%%------------------------------------------------------------------------------
+%% @doc Função que contitui o ciclo de vida de um Sender.
+%%
+%% Reencaminha cada uma das mensagens vindas dos Atores Sessão dos clientes,
+%% para a exchange associada.
+%% @end
+%%------------------------------------------------------------------------------
 msg_forwarding_loop(Socket) ->
 	receive
 		{msg, Msg, From} ->
